86c5bc3c8f0d1444a5bc20baa1663504ef8c9ee4
BobNarwhalKing <danceflurry4366@gmail.com>
2019-06-05 16:34:03 -0500

    d

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
-  # add controller methods
+ set :views, './app/views/figures'
+  get "/figures" do
+    @figures = Figure.all
+    erb :index
+  end
+
+  get "/figures/new" do
+    erb :new
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :show
+  end
+
+  post "/figures" do
+    binding.pry
+    @figure = Figure.create(name: params["figure"]["name"])
+    unless params["figure"]["new_landmark_name"].empty?
+      @figure.landmarks << Landmark.create(name: params["figure"]["new_landmark_name"], year_completed: params["figure"]["new_landmark
+      _year"])
+    end
+  end
+
+ 
+
 end
